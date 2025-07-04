03658209fe20df758ccf2e4350da72d76fe03d68
HentaiSimpLord <pierrefrancois1907@gmail.com>
2025-07-04 22:53:29 +0200

    test update

--- src/Components/Quote-Box.jsx
+++ src/Components/Quote-Box.jsx
@@ -1,45 +1,56 @@
-import '../styles/App.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { setQuote } from '../redux/action';
-import React, { useEffect } from 'react';
-import quotes from '../data/quotes.json';
-
+import "../styles/App.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { setQuote } from "../redux/action";
+import React, { useEffect } from "react";
+import quotes from "../data/quotes.json";
 
 const QuoteBox = ({ backgroundColor }) => {
-    const { text, author } = useSelector((state) => state);
-    const dispatch = useDispatch();
-    
-    const handleNewQuote = async () => {
-        const random = quotes[Math.floor(Math.random() * quotes.length)];
+  const { text, author } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-        dispatch(
-            setQuote({
-                text: random.text,
-                author: random.author
-            })
-        );
-    };
+  const handleNewQuote = async () => {
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
 
-    useEffect(() => {
-        handleNewQuote();
-    }, [])
-
-    const tweetQuote = `https://x.com/intent/tweet?text=${
-        encodeURIComponent(`"${text}" – ${author}`)
-    }`
-
-    return (
-        <div className='QuoteApp' style={{ backgroundColor: backgroundColor }}>
-            <div id='quote-box'>
-                <span id='text' style={{ color: backgroundColor }}>{ text }</span>
-                <span id='author' style={{ color: backgroundColor }}>{ author }</span>
-                <div className='container'>
-                    <a id='tweet-quote' target='_top' href={ tweetQuote }>X</a>
-                    <button id='new-quote' style={{ backgroundColor: backgroundColor }} onClick={handleNewQuote}>New Quote</button>
-                </div>
-            </div>
-        </div>
+    dispatch(
+      setQuote({
+        text: random.text,
+        author: random.author,
+      })
     );
-}
+  };
+
+  useEffect(() => {
+    handleNewQuote();
+  }, []);
+
+  const tweetQuote = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    `"${text}" – ${author}`
+  )}`;
+
+  return (
+    <div className="QuoteApp" style={{ backgroundColor: backgroundColor }}>
+      <div id="quote-box">
+        <span id="text" style={{ color: backgroundColor }}>
+          {text}
+        </span>
+        <span id="author" style={{ color: backgroundColor }}>
+          {author}
+        </span>
+        <div className="container">
+          <a id="tweet-quote" target="_top" href={tweetQuote}>
+            X
+          </a>
+          <button
+            id="new-quote"
+            style={{ backgroundColor: backgroundColor }}
+            onClick={handleNewQuote}
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default QuoteBox;
